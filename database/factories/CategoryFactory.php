ba1b6da729220dec4af6f01aa3910fd05c2902ec
FanX0 <faridazhari46@gmail.com>
2024-03-09 20:43:02 +0700

    add Str slug category

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -21,8 +21,8 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
-            'slug' => $this->faker->slug(),
+            'name' => $name = $this->faker->name(),
+            'slug' => Str::slug($name),
         ];
     }
 }
