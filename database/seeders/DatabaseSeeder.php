f41ed82a5efda14f65d9942dedf2a165f40d54b2
FanX0 <faridazhari46@gmail.com>
2024-03-09 19:13:32 +0700

    add factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Cart;
+use App\Models\ShippingAddress;
+use App\Models\Transaction;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,7 +23,11 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('admin321')
         ]);
 
-        \App\Models\User::factory(10)->for(Cart::factory())->create();
+        User::factory(10)
+            ->has(Cart::factory()->count(3)) // Mengaitkan 3 keranjang dengan setiap pengguna yang dibuat
+            ->has(ShippingAddress::factory())
+            ->has(Transaction::factory())
+            ->create();
 
        
     }
