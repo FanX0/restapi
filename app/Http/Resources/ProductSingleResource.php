f7f3d597ccdac9a6e4802d35a74fe6feaea61540
FanX0 <faridazhari46@gmail.com>
2024-03-10 14:21:29 +0700

    add picture

--- app/Http/Resources/ProductSingleResource.php
+++ app/Http/Resources/ProductSingleResource.php
@@ -22,6 +22,7 @@ class ProductSingleResource extends JsonResource
             'price'=>number_format($this->price,0,'.','.'),
             'actual_price'=>$this->price,
             'created'=>$this->created_at->format("d F, Y"),
+            'picture'=>$this->picture,
         ];
     }
 }
