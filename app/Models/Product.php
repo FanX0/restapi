7048b7865db6f98a87c842bfac02329ed0a21e1f
FanX0 <faridazhari46@gmail.com>
2024-03-10 14:21:51 +0700

    add guarded

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,11 +11,7 @@ class Product extends Model
 {
     use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    protected $guarded = [];
 
     public function getRouteKeyName()
     {
